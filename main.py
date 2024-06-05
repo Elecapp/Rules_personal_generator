948a13c887ac8252379f1423ebb1936efc059f6f
Eleonora Cappuccio <eleonora.cappuccio@phd.unipi.it>
2024-06-05 10:56:10 +0200

    WIP:trying to generate rule

--- main.py
+++ main.py
@@ -15,16 +15,93 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
 from sklearn.compose import make_column_selector as selector
-
+import random
 from lore_sa.dataset import TabularDataset
+from lore_sa.bbox import sklearn_classifier_bbox
 from lore_sa.surrogate import DecisionTreeSurrogate
+from lore_sa.encoder_decoder import EncDec
+from lore_sa.lore import TabularRandomGeneratorLore
+
+
+class IdentityEncoder(EncDec):
+    """
+    It provides an interface to access Identity encoding functions.
+    """
+    def __init__(self,dataset_descriptor):
+        super().__init__(dataset_descriptor)
+
+    def encode(self, x: np.array) -> np.array:
+        """
+        It applies the encoder to the input features
+
+        :param [Numpy array] x: Array to encode
+        :return [Numpy array]: Encoded array
+        """
+        return x
+
+    def get_encoded_features(self):
+        """
+        Provides a dictionary with the new encoded features name and the new index
+        :return:
+        """
+        return self.encoded_features
+
+    def decode(self, x: np.array) -> np.array:
+        return x
+
+    def decode_target_class(self, x: np.array):
+        return x
+
+class NewGen:
+    def __init__(self):
+        self.gen_data = None
+
+    def perturb(self, x, num_instances):
+        perturbed_arrays = []
+
+        choices = [0, 1, 2, 3, 4]
+
+        covid_weights = [
+            [0.0, 0.25, 0.13, 0.36, 0.26],  # 0
+            [0.0, 0.40, 0.30, 0.20, 0.10],  # 1
+            [0.0, 0.25, 0.40, 0.25, 0.10],  # 2
+            [0.0, 0.10, 0.25, 0.40, 0.25],  # 3
+            [0.0, 0.10, 0.20, 0.30, 0.40],  # 4
+        ]
+
+        mobility_weights = [
+            [0.0, 0.12, 0.18, 0.45, 0.24],  # 0
+            [0.0, 0.40, 0.30, 0.20, 0.10],  # 1
+            [0.0, 0.25, 0.40, 0.25, 0.10],  # 2
+            [0.0, 0.10, 0.25, 0.40, 0.25],  # 3
+            [0.0, 0.10, 0.20, 0.30, 0.40]  # 4
+
+        ]
+
+        for _ in range(num_instances):
+            perturbed_arr = x.copy()
+
+            for val in range(0, 3):  # covid
+                perturbed_arr[val] = random.choices(choices, weights=covid_weights[val])[0]
+
+            for val in range(3, 7):  # mobility
+                perturbed_arr[val] = random.choices(choices, weights=mobility_weights[val - 3])[0]
+
+            perturbed_arr[7] = random.choice(range(42, 442, 7))
+            perturbed_arr[8] = random.choice(range(7, 148, 7))
+
+            perturbed_arrays.append(perturbed_arr)
+        self.gen_data = perturbed_arrays
+        return np.array(perturbed_arrays)
+
+
 
 
 def load_data_from_csv():
-    df = pd.read_csv("../../datasets/Final_data.csv")
+    df = pd.read_csv("datasets/Final_data.csv")
 
-    df = df.loc[:, ['Class_label', 'Week5_Covid', 'Week4_Covid', 'Week3_Covid', 'Week5_Mobility', 'Week4_Mobility',
-                    'Week3_Mobility', 'Week2_Mobility', 'Days_passed', 'Duration']]
+    df = df.loc[:, ['Week5_Covid', 'Week4_Covid', 'Week3_Covid', 'Week5_Mobility', 'Week4_Mobility',
+                    'Week3_Mobility', 'Week2_Mobility', 'Days_passed', 'Duration','Class_label']]
 
     mask = df.drop(["Class_label"], axis=1).isna().all(axis=1) & df['Class_label'].notna()
     result_df = df[~mask]
@@ -35,11 +112,7 @@ def load_data_from_csv():
 
 def create_and_train_model(result_df):
     y = result_df["Class_label"].values
-    X_feat = result_df.loc[:, 'Week5_Covid':'Duration']
-    numerical_columns_selector = selector(dtype_exclude=object)
-    categorical_columns_selector = selector(dtype_include=object)
-    numerical_columns = numerical_columns_selector(X_feat)
-    categorical_columns = categorical_columns_selector(X_feat)
+    X_feat = result_df.loc[:, 'Week5_Covid':'Duration'].values
     covid_categories = ['NONE', 'c1', 'c2', 'c3', 'c4']
     mobility_categories = ['NONE', 'm1', 'm2', 'm3', 'm4']
     enc = OrdinalEncoder(
@@ -48,8 +121,8 @@ def create_and_train_model(result_df):
     numerical_preprocessor = StandardScaler()
     preprocessor = ColumnTransformer(
         [
-            ("ordinal-encoder", enc, categorical_columns),
-            ("standard_scaler", numerical_preprocessor, numerical_columns),
+            ("ordinal-encoder", enc, list(range(0, 7))),
+            ("standard_scaler", numerical_preprocessor, list(range(7, 9))),
         ]
     )
     clf = RandomForestClassifier(n_estimators=100, random_state=0)
@@ -60,19 +133,95 @@ def create_and_train_model(result_df):
     )
     _ = model.fit(data_train, target_train)
 
-    y_predict = model.predict(data_test)
-
     print(model.score(data_test, target_test))
 
-    return data_train, clf
 
-def generate_data_from_instance():
+    return model
+
+
+def new_lore():
     res = load_data_from_csv()
+
+    model = create_and_train_model(res)
+    instance = res.values[5, : -1]
+    print(instance)
+    prediction = model.predict([instance])
+
+    print(prediction)
+    bbox = sklearn_classifier_bbox.sklearnBBox(model)
+    data = TabularDataset(data=res,class_name='Class_label')
+    x = data.df.iloc[5,:-1].values
+    print(x)
+    lore = TabularRandomGeneratorLore(bbox, data)
+    print(data.descriptor)
+    rule = lore.explain(x)
+    print(rule)
+
+
+
+
+
+
+
+def generate_neigh_from_instance():
+    res = load_data_from_csv()
+    model = create_and_train_model(res)
+    instance = res.values[5, 1:]
+    #print(instance)
+    prediction = model.predict([instance])
+    #print(prediction)
+
+    cTransformer = model.named_steps.get('columntransformer')
+    clf = model.named_steps.get('randomforestclassifier')
+    gen = NewGen()
+    neighb = gen.perturb(cTransformer.transform([instance])[0],100)
+    arrays = []
+    for name, indices in cTransformer.output_indices_.items():
+        transformer = cTransformer.named_transformers_.get(name)
+        arr = neighb[:, indices.start: indices.stop]
+
+        #if transformer in (None, 'passthrough', 'drop'):
+        #    pass
+        #else:
+        #    arr = transformer.inverse_transform(arr)
+
+        arrays.append(arr)
+
+    retarr = np.concatenate(arrays, axis=1)
+
+    # bbox = sklearn_classifier_bbox.sklearnBBox(clf)
+    #Add the predicted class to the neighbourhood array
+    neigh_class_pre = clf.predict(retarr)
+    neighbourhood_class = np.hstack((retarr, neigh_class_pre.reshape(-1, 1)))
+    covid_categories = [['NONE', 'c1', 'c2', 'c3', 'c4']]
+
+    enc = OrdinalEncoder(categories=covid_categories)
+    last_column = neighbourhood_class[:, -1].reshape(-1, 1)
+    encoded_last_column = enc.fit_transform(last_column)
+    neighbourhood_class[:, -1] = encoded_last_column.flatten()
+
+    #create the surrogate
+    neighbourhood_class = neighbourhood_class.astype(int)
+    surrogate = DecisionTreeSurrogate()
+    surrogate.train(neighbourhood_class[:,:-1], neighbourhood_class[:,-1])
+
+    #generate rule
+    x_n = neighbourhood_class[5,:-1]
+    #rule = surrogate.get_rule(x_n, neighbourhood_class)
+    #print(rule)
+
+
+
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    generate_data_from_instance()
+    new_lore()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
