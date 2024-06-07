05414be90151adecda602d82129396683ff1521c
Eleonora Cappuccio <eleonora.cappuccio@phd.unipi.it>
2024-06-07 15:25:50 +0200

    WIP: generating a Plot for visualize distances

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer, LabelEncoder
-# from sklearn import metrics
-
+from sklearn.preprocessing import OrdinalEncoder
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
-from sklearn.compose import make_column_selector as selector
 import random
 from lore_sa.dataset import TabularDataset, Dataset
 from lore_sa.bbox import sklearn_classifier_bbox, AbstractBBox
+from lore_sa.neighgen import RandomGenerator
 from lore_sa.neighgen.neighborhood_generator import NeighborhoodGenerator
 from lore_sa.surrogate import DecisionTreeSurrogate
 from lore_sa.encoder_decoder import EncDec, ColumnTransformerEnc
 from lore_sa.lore import TabularRandomGeneratorLore, Lore
+
+from sklearn.metrics import pairwise_distances
+
+import altair as alt
+
+alt.data_transformers.enable('default', max_rows=None)
 
 
 class IdentityEncoder(EncDec):
     """
     It provides an interface to access Identity encoding functions.
     """
-    def __init__(self,dataset_descriptor):
+
+    def __init__(self, dataset_descriptor):
         super().__init__(dataset_descriptor)
 
     def encode(self, x: np.array) -> np.array:
@@ -54,11 +56,11 @@ class IdentityEncoder(EncDec):
         return x
 
 
-
 class ProbabilitiesWeightBasedGenerator(NeighborhoodGenerator):
 
-    def __init__(self, bbox: AbstractBBox, dataset:Dataset, encoder:EncDec, ocr=0.1):
+    def __init__(self, bbox: AbstractBBox, dataset: Dataset, encoder: EncDec, ocr=0.1):
         super().__init__(bbox, dataset, encoder, ocr)
+        self.neighborhood = None
 
     def generate(self, x: np.array, num_instances: int, descriptor: dict, encoder):
 
@@ -98,23 +100,19 @@ class ProbabilitiesWeightBasedGenerator(NeighborhoodGenerator):
             perturbed_arrays.append(perturbed_arr)
 
         # save to png
-        return np.array(perturbed_arrays)
+        self.neighborhood = np.array(perturbed_arrays)
+
+        return self.neighborhood
 
     def check_generated(self, filter_function=None, check_fuction=None):
         pass
-
-
-
-
-
-
 
 
 def load_data_from_csv():
     df = pd.read_csv("datasets/Final_data.csv")
 
     df = df.loc[:, ['Week5_Covid', 'Week4_Covid', 'Week3_Covid', 'Week5_Mobility', 'Week4_Mobility',
-                    'Week3_Mobility', 'Week2_Mobility', 'Days_passed', 'Duration','Class_label']]
+                    'Week3_Mobility', 'Week2_Mobility', 'Days_passed', 'Duration', 'Class_label']]
 
     mask = df.drop(["Class_label"], axis=1).isna().all(axis=1) & df['Class_label'].notna()
     result_df = df[~mask]
@@ -122,6 +120,7 @@ def load_data_from_csv():
     result_df = result_df.fillna("NONE")
 
     return result_df
+
 
 def create_and_train_model(result_df):
     y = result_df["Class_label"].values
@@ -146,10 +145,75 @@ def create_and_train_model(result_df):
     )
     _ = model.fit(data_train, target_train)
 
+    encoded_train = preprocessor.fit_transform(data_train)
+    df = pd.DataFrame(encoded_train)
+    df.columns = result_df.columns[:-1]
+    df.iloc[:, -2:] = result_df.iloc[:, -3:-1]
+    df['Class_label'] = result_df['Class_label']
+    print(df)
+
     print(model.score(data_test, target_test))
 
-
     return model
+
+
+def compute_statistics_distance():
+    res = load_data_from_csv()
+    model = create_and_train_model(res)
+    bbox = sklearn_classifier_bbox.sklearnBBox(model)
+    data = TabularDataset(data=res, class_name='Class_label')
+    x = data.df.iloc[355, :-1].values
+    encoder = ColumnTransformerEnc(data.descriptor)
+
+
+    # We transform the input data for an OrdinalEncoder, as used in the model.
+    X_feat = res.loc[:, 'Week5_Covid':'Duration']
+    data_transf = model.named_steps.get('columntransformer').transform(X_feat)
+    data_df = pd.DataFrame(data_transf,columns=X_feat.columns)
+
+    data_wrapper = TabularDataset(data=data_df, )
+
+
+    iEncoder = IdentityEncoder(data.descriptor)
+    generator = ProbabilitiesWeightBasedGenerator(bbox, data, iEncoder)
+    rnd_generator = RandomGenerator(bbox, data, encoder)
+
+    print('Computing distances of custom generator')
+    np_mins = measure_distances(data, encoder, generator, x)
+    print('custom generator', np_mins)
+
+    print('Computing distances of random generator')
+    rnd_np_mins = measure_distances(data, encoder, rnd_generator, x)
+    print('random generator', rnd_np_mins)
+    alt_df_dist_o = pd.DataFrame(np_mins, columns=['Distance'])
+    alt_df_dist_r = pd.DataFrame(rnd_np_mins, columns=['Distance'])
+    print(alt_df_dist_o.head(10))
+    alt_df_dist_o['source'] = 'our'
+    alt_df_dist_r['source'] = 'random'
+    boxplot_df = pd.concat([alt_df_dist_o, alt_df_dist_r], axis=0)
+    box_plot = alt.Chart(boxplot_df).mark_boxplot().encode(
+        alt.X("Distance:Q").scale(zero=False),
+        alt.Y("source:N"),
+        alt.Color("source:N")
+    ).properties(
+        height=300,
+        width=500,
+        title='Distances with the original Data'
+    )
+    box_plot.save('plot/boxplot.png')
+
+def measure_distances(data, encoder, generator, x):
+    global_mins = []
+    for i in range(10):
+        neighb = generator.generate(encoder.encode(x.reshape(1, -1))[0], 1000, data.descriptor, encoder)
+        dists = calculate_distance(neighb, encoder.encoder.transform(data.df.iloc[:, :-1].values))
+        local_mins = np.min(dists, axis=1)
+        global_mins.append(local_mins)
+        if i % 100 == 0:
+            print(f"{i}...\r")
+    print()
+    np_mins = np.average(np.array(global_mins), axis=0)
+    return np_mins
 
 
 def new_lore():
@@ -165,11 +229,11 @@ def new_lore():
     data = TabularDataset(data=res, class_name='Class_label')
     x = data.df.iloc[355, :-1].values
     print(x)
-    lore = TabularRandomGeneratorLore(bbox,data)
+    # lore = TabularRandomGeneratorLore(bbox, data)
     encoder = ColumnTransformerEnc(data.descriptor)
     surrogate = DecisionTreeSurrogate()
     generator = ProbabilitiesWeightBasedGenerator(bbox, data, encoder)
-    proba_lore = Lore(bbox, data, encoder, generator,surrogate)
+    proba_lore = Lore(bbox, data, encoder, generator, surrogate)
 
     print(data.descriptor)
     rule = proba_lore.explain(x)
@@ -181,74 +245,23 @@ def new_lore():
         print(cr)
         print('-----')
 
-
-    #for i in range(nrows):
+    # for i in range(nrows):
     #    rl = proba_lore.explain(data.df.iloc[i, :-1].values)
     #    if len(rl['counterfactuals']) > 0:
     #        print(i)
 
 
+def calculate_distance(X1: np.array, X2: np.array, y_1: np.array = None, y_2: np.array = None):
+    dists = pairwise_distances(X1, X2, metric='euclidean')
+
+    return dists
 
 
-
-'''
-def generate_neigh_from_instance():
-    res = load_data_from_csv()
-    model = create_and_train_model(res)
-    instance = res.values[5, 1:]
-    #print(instance)
-    prediction = model.predict([instance])
-    #print(prediction)
-
-    cTransformer = model.named_steps.get('columntransformer')
-    clf = model.named_steps.get('randomforestclassifier')
-    gen = NewGen()
-    neighb = gen.perturb(cTransformer.transform([instance])[0],100)
-    arrays = []
-    for name, indices in cTransformer.output_indices_.items():
-        transformer = cTransformer.named_transformers_.get(name)
-        arr = neighb[:, indices.start: indices.stop]
-
-        #if transformer in (None, 'passthrough', 'drop'):
-        #    pass
-        #else:
-        #    arr = transformer.inverse_transform(arr)
-
-        arrays.append(arr)
-
-    retarr = np.concatenate(arrays, axis=1)
-
-    # bbox = sklearn_classifier_bbox.sklearnBBox(clf)
-    #Add the predicted class to the neighbourhood array
-    neigh_class_pre = clf.predict(retarr)
-    neighbourhood_class = np.hstack((retarr, neigh_class_pre.reshape(-1, 1)))
-    covid_categories = [['NONE', 'c1', 'c2', 'c3', 'c4']]
-
-    enc = OrdinalEncoder(categories=covid_categories)
-    last_column = neighbourhood_class[:, -1].reshape(-1, 1)
-    encoded_last_column = enc.fit_transform(last_column)
-    neighbourhood_class[:, -1] = encoded_last_column.flatten()
-
-    #create the surrogate
-    neighbourhood_class = neighbourhood_class.astype(int)
-    surrogate = DecisionTreeSurrogate()
-    surrogate.train(neighbourhood_class[:,:-1], neighbourhood_class[:,-1])
-
-    #generate rule
-    x_n = neighbourhood_class[5,:-1]
-    #rule = surrogate.get_rule(x_n, neighbourhood_class)
-    #print(rule)
-'''
-
+def project_and_plot_data(X: np.array, y: np.array = None):
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     new_lore()
-
-
-
-
-
-
-
+    compute_statistics_distance()
