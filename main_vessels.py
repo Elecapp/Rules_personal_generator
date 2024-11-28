2fe699089761b81a76f01816446790df0707d089
Eleonora Cappuccio <eleonora.cappuccio@phd.unipi.it>
2024-11-28 14:50:07 +0100

    WIP reconstructed instance path in the DT

--- main_vessels.py
+++ main_vessels.py
@@ -8,8 +8,10 @@ from datetime import datetime, timedelta
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import make_column_selector as selector
+from sklearn import tree
 from sklearn.manifold import TSNE
 
 import umap.umap_ as umap
@@ -36,6 +38,26 @@ from lore_sa.lore import TabularRandomGeneratorLore, Lore
 
 from sklearn.metrics import pairwise_distances
 
+class GenerateDecisionTrees:
+    def __init__(self, test_size=0.3, random_state=42):
+        self.random_state = random_state
+        self.test_size = test_size
+        self.classifiers = {}
+
+    def decision_trees(self,X_feat, y):
+        self.classifiers = {}
+        for target_class in np.unique(y):
+            binary_y = (y == target_class).astype(int)
+            X_train, X_test, y_train, y_test = train_test_split(X_feat, binary_y, random_state=self.random_state, test_size=self.test_size)
+            clf = DecisionTreeClassifier(random_state=self.random_state)
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            # print(f"Classification Report for class {target_class}:\n")
+            # print(classification_report(y_test, y_pred))
+            self.classifiers[target_class] = clf
+        return self.classifiers
+
+
 
 class NewGen(NeighborhoodGenerator):
     def __init__(self, bbox: AbstractBBox, dataset: Dataset, encoder: EncDec, ocr=0.1):
@@ -44,14 +66,13 @@ class NewGen(NeighborhoodGenerator):
         self.preprocess = bbox.bbox.named_steps.get('columntransformer')
         self.gen_data = None
 
+
     def generate(self, x, num_instances:int=1000, descriptor: dict=None, encoder=None):
         perturbed_arrays = []
         perturbed_arrays.append(x.copy())
         for _ in range(num_instances):
             # make a deep copy of x in the variable perturbed_arr
             perturbed_arr = x.copy()
-
-
             perturbed_arr[0] = random.uniform(0, 17.93)
             perturbed_arr[1] = random.uniform(perturbed_arr[0], 20.12)  # always greater than minspeed
             perturbed_arr[2] = random.uniform(perturbed_arr[1], 20.75)  # always greater than speedQ1
@@ -62,11 +83,94 @@ class NewGen(NeighborhoodGenerator):
             perturbed_arr[7] = random.uniform(0.12, 282.26)
             perturbed_arr[8] = math.log10(random.uniform(math.exp(-3.05), perturbed_arr[
                 7]))  # inverse log transform, identify value less than max dist, then log transform again
-
             perturbed_arrays.append(perturbed_arr[:])
-
         self.gen_data = perturbed_arrays
         self.neighborhood = encoder.encode(perturbed_arrays)
+
+    def perturbate(self, instance, bb, classifiers):
+        class_label = bb.predict([instance])[0]
+        chosen_dt = classifiers[class_label]
+        fig = plt.figure(figsize=(25, 20))
+        _ = tree.plot_tree(chosen_dt, feature_names=X_feat.columns, filled=True)
+        fig.savefig("plot/decision_tree.png")
+        # Get the decision path and leaf node for the instance
+        decision_path = chosen_dt.decision_path(instance.reshape(1,-1))
+        leaf_id = chosen_dt.apply(instance.reshape(1,-1))[0]
+
+        feature = chosen_dt.tree_.feature
+        threshold = chosen_dt.tree_.threshold
+        # Extract influencing features based on the decision path
+        node_indices = decision_path.indices  # Nodes visited along the path
+        influencing_features = []
+
+        for node in node_indices:
+            if feature[node] != -2:  # -2 indicates a leaf node
+                influencing_features.append(
+                    (X_feat.columns[feature[node]], threshold[node], instance[feature[node]], instance[feature[node]] < threshold[node])
+                )
+
+        return decision_path, leaf_id, feature, threshold, influencing_features
+
+
+
+
+
+
+
+        '''
+        in the class there are already the stored decision tree, 
+        i retrieve the decision tree
+        '''
+        '''
+        New alg description:
+        - check the class label of x
+        - find the corresponding decision tree for the class label
+        - find the corresponding branch in the DT for the instance
+        - generate the perturbations by altering feature values to 
+          fit into other branches of the DT (threshold of same class vs different class) and ensure
+          that the soft rules are maintained
+        - Keep the feature that do not influence the class decision as constants.
+        
+        ---------------
+        def create_and_train_model(df):
+            return bbox
+        #create class with the precalc decision tree
+        def decision_trees(x)
+            classifiers = {}
+            for target_class in np.unique(y):
+                binary_y = (y == target_class).astype(int)
+                X_train, X_test, y_train, y_test = train_test_split(X_feat, binary_y, test_size=0.3, random_state=42)
+                clf = DecisionTreeClassifier(random_state=42)
+                clf.fit(X_train, y_train)
+                y_pred = clf.predict(X_test)
+                #print(f"Classification Report for class {target_class}:\n")
+                #print(classification_report(y_test, y_pred))
+                classifiers[target_class] = clf
+            return classifiers
+        x = [instance]
+        x_class= bbox.predict(x) #check class label for x
+        chosen_dt = classifiers[x_class] # find the corresponding DT
+        check_x = x.reshape(1, -1)
+        # Step 1: Get the decision path
+        node_indicator = clf.decision_path(check_x)
+
+        # Step 2: Find the leaf node for the instance
+        leaf_id = clf.apply(check_x)[0]
+
+        # Step 3: Extract conditions along the path
+        feature = clf.tree_.feature
+        threshold = clf.tree_.threshold
+        #generate the perturbations by altering feature values to fit into other branches of the DT (threshold of same 
+        class vs different class)
+        #Keep the feature that do not influence the class decision as constants
+        candidate_neigh = [x.perturb] # perturb everything but the irrelevant feature, leverage on the def generate
+        check what is inside node indicator.
+        for n in candidate_neigh: #ensure that the soft rules are maintained
+            check if it fits the soft rules
+            if not:
+                modify accordingly
+            proper_neigh   
+        '''
 
         return self.neighborhood
 
@@ -123,7 +227,7 @@ def create_and_train_model(df):
 
     y_predict = model.predict(data_test)
     print(model.score(data_test, target_test))
-    return model
+    return model, X_feat, y
 
 
 def new_lore(data, bb):
@@ -132,18 +236,26 @@ def new_lore(data, bb):
     data = data.loc[:, features]
     #instance = data.values[5, : -1]
     #prediction = model.predict([instance])
-    instance = data.iloc[10, :-1].values
+    instance = data.iloc[50, :-1].values
     ds = TabularDataset(data=data, class_name='class N', categorial_columns=['class N'])
     bbox = sklearn_classifier_bbox.sklearnBBox(bb)
     print(bb.predict([instance]))
 
     print("instance is:", instance)
     x = instance
+
+
     #print('model prediction is', model.predict([x]))
     #lore = TabularRandomGeneratorLore(bbox, x)
     encoder = ColumnTransformerEnc(ds.descriptor)
     surrogate = DecisionTreeSurrogate()
     generator = NewGen(bbox, ds, encoder)
+    dt_model = GenerateDecisionTrees(test_size=0.3, random_state=42)
+    trained_dt = dt_model.decision_trees(X_feat, y)
+    path = generator.perturbate(x, bbox, trained_dt)
+
+
+
     proba_lore = Lore(bbox, ds, encoder, generator, surrogate)
     print('Prediction:', bb.predict([x]))
 
@@ -160,7 +272,6 @@ def new_lore(data, bb):
 
 if __name__ == '__main__':
     res = load_data_from_csv()
-    model = create_and_train_model(res)
+    model, X_feat, y = create_and_train_model(res)
     print(model)
-
     new_lore(res, model)
