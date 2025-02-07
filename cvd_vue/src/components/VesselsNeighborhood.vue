8593dd2bc3c17b876140aa1fb14c06593dce4c3f
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2025-02-07 18:05:57 +0100

    [Vue.js] Introducing the webforms for the visualization

---
+++ cvd_vue/src/components/VesselsNeighborhood.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-row>
+      <h1>Vessels Neighborhood Explorer</h1>
+      <p></p>
+    </b-row>
+    <b-row>
+      <b-form>
+        <b-form-group id="SpeedMinimum-g" label-for="SpeedMinimum-i"
+        description="The minimum speed in the period">
+          <label for="SpeedMinimum-i">Speed: {{form.SpeedMinimum}}</label>
+          <b-form-input id="SpeedMinimum-i" v-model="form.SpeedMinimum" required
+          type="range" :min="0" :max="20" :step="0.1">
+
+          </b-form-input>
+        </b-form-group>
+      </b-form>
+    </b-row>
+    <b-form-textarea id="json-input" placeholder="Insert a JSON object"
+                     rows="15"></b-form-textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VesselsNeighborhood',
+  data() {
+    return {
+      form: {
+        SpeedMinimum: 0,
+        SpeedQ1: 0,
+        SpeedMedian: 0,
+        SpeedQ3: 0,
+        DistanceStartShapeCurvature: 0,
+        DistStartTrendAngle: 0,
+        DistStartTrendDevAmplitude: 0,
+        MaxDistPort: 0,
+        MinDistPort: 0,
+      },
+    };
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
--- cvd_vue/src/router/index.js
+++ cvd_vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import PatientClassifier from '@/components/PatientClassifier';
+import VesselsNeighborhood from '../components/VesselsNeighborhood.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'PatientClassifier',
       component: PatientClassifier,
+    },
+    {
+      path: '/vessels-neighborhood',
+      name: 'VesselsNeighborhood',
+      component: VesselsNeighborhood,
     },
   ],
 });
